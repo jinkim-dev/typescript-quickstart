61a60af1bea63dad6bedf5b771736a9d5bd5c715
MINJI KIM <rlaalswl627@gmail.com>
2020-04-08 22:57:03 +0900

    Add: 클래스와 인터페이스 예제 추가

---
+++ ch7/class/src/class/modifier/ploymorphism-class.ts
@@ -0,0 +1,30 @@
+// 부모 클래스와 자식 클래스 간의 다형성
+class Planet {
+    public diameter: number;    // 지금
+    protected isTransduction: boolean = true;   // 공전
+
+    getIsTransduction(): boolean {
+        return this.isTransduction;
+    }
+
+    stopTransduction(): void {
+        console.log("stop1");
+        this.isTransduction = false;
+    }
+}
+
+class Earth extends Planet {
+    public features: string[] = ["soil", "water", "oxyzen"];
+    stopTransduction(): void {
+        console.log("stop2");
+        this.isTransduction = false;
+    }
+}
+
+let earth: Planet = new Earth();
+earth.diameter = 12656.2;
+console.log("1번 : " + earth.diameter);
+console.log("2번 : " + earth.getIsTransduction());
+earth.stopTransduction();
+console.log("3번 : " + earth.getIsTransduction());
+// console.log(earth.features);     // 접근 불가
