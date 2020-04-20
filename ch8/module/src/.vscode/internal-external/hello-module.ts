c91ab40a9f950256eeeeecf75bbe3643ca6acdb0
MINJI KIM <rlaalswl627@gmail.com>
2020-04-20 23:19:07 +0900

    Added: 모듈 예제 추가 - 네임스페이스에 대한 이해

---
+++ ch8/module/src/.vscode/internal-external/hello-module.ts
@@ -0,0 +1,6 @@
+// Hello 내부 모듈(module 키워드 사용)
+module Hello {
+    function print() {
+        console.log("hello!");
+    }
+}
---
+++ ch8/module/src/.vscode/internal-external/hello-namespace.ts
@@ -0,0 +1,6 @@
+// Hello 내부 모듈(namespace 키워드 사용)
+namespace Hello {
+    function print() {
+        console.log("hello!");
+    }
+}
